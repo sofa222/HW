e0ce7f7c270258df83bd435c30513f2ae315b2cc
sofa222 <74463470+sofa222@users.noreply.github.com>
2020-12-25 21:46:21 +0300

    Update 5.py

--- 5.py
+++ 5.py
@@ -43,32 +43,31 @@ class DequeTest(unittest.TestCase):
                 testing_action, certified_action = self.random_put_action()
                 testing_action(value)
                 certified_action(value)
-                size +=1
+                size += 1
             else:
                 testing_action, certified_action = self.random_get_action()
                 testing_value = testing_action()
                 certified_value = certified_action()
                 size -= 1
                 self.assertEqual(testing_value, certified_value)
-                self.assertEqual(self.testing_deque.size, size)
+                self.assertEqual(self.testing_deque.size(), size)
 
     def test_empty_deque_exception(self):
         d = Deque()
         self.assertRaises(Exception, d.pop_front)
 
     def test_manual(self):
-    	self.testing_deque.push_back(1)
-    	self.testing_deque.push_back(2)
-    	self.testing_deque.push_back(3)
+        self.testing_deque.push_back(1)
+        self.testing_deque.push_back(2)
+        self.testing_deque.push_back(3)
 
-    	self.assertEqual(1, self.testing_deque.pop_front())
-    	self.assertEqual(2, self.testing_deque.pop_front())
-    	
-    	self.testing_deque.push_front(4)
-    	
-    	self.assertEqual(3, self.testing_deque.pop_back())
-    	self.assertEqual(4, self.testing_deque.pop_back())
+        self.assertEqual(1, self.testing_deque.pop_front())
+        self.assertEqual(2, self.testing_deque.pop_front())
 
+        self.testing_deque.push_front(4)
+
+        self.assertEqual(3, self.testing_deque.pop_back())
+        self.assertEqual(4, self.testing_deque.pop_back())
 
 
 if __name__ == "__main__":
