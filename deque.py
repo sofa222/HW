d7e2497a138895fa84313ef845ea9f28bd78d188
sofa222 <74463470+sofa222@users.noreply.github.com>
2020-12-25 21:46:52 +0300

    Update deque.py

--- deque.py
+++ deque.py
@@ -1,77 +1,76 @@
 class Node:
-	def __init__(self, value):
-		self.value = value
-		self.next = None
-		self.prev = None
+
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
 
 class Deque:
 
+    def __init__(self):
+        self.__size = 0
+        self.front = None
+        self.back = None
 
-	def __init__(self):
-		self.size = 0
-		self.front = None
-		self.back = None
+    def push_back(self, value):
+        n = Node(value)
+        self.__size += 1
 
-	def push_back(self, value):
-		n = Node(value)
+        if self.is_empty():
+            self.back = n
+            self.front = n
+            return
 
-		if self.is_empty():
-			self.back = n
-			self.front = n
-			self.size += 1
-			return
+        n.next = self.back
+        self.back.prev = n
+        self.back = n
 
-		n.next = self.back
-		self.back.prev = n
-		self.back = n
-		self.size += 1
+    def push_front(self, value):
+        n = Node(value)
+        self.__size += 1
 
-	def push_front(self, value):
-		n = Node(value)
+        if self.is_empty():
+            self.back = n
+            self.front = n
+            return
 
-		if self.is_empty():
-			self.back = n
-			self.front = n
-			self.size += 1
-			return
+        self.front.next = n
+        n.prev = self.front
+        self.front = n
 
-		self.front.next = n
-		n.prev = self.front
-		self.front = n
-		self.size += 1
+    def pop_back(self):
+        if self.is_empty():
+            raise Exception("Deque is empty.")
 
-	def pop_back(self):
-		if self.is_empty():
-			raise Exception("Deque is empty.")
+        n = self.back
+        self.__size -= 1
 
-		n = self.back
-		self.size -= 1
+        if n.next is None:
+            self.back = None
+            return n.value
 
-		if n.next is None:
-			self.back = None
-			return n.value
+        self.back = n.next
+        self.back.prev = None
+        return n.value
 
-		self.back = n.next
-		self.back.prev = None
-		return n.value
+    def pop_front(self):
+        if self.is_empty():
+            raise Exception("Deque is empty.")
 
-	def pop_front(self):
-		if self.is_empty():
-			raise Exception("Deque is empty.")
+        n = self.front
+        self.__size -= 1
 
-		n = self.front
-		self.size -= 1
+        if n.prev is None:
+            self.front = None
+            return n.value
 
-		if n.prev is None:
-			self.front = None
-			return n.value
+        self.front = n.prev
+        self.front.next = None
+        return n.value
 
-		self.front = n.prev
-		self.front.next = None
-		return n.value
+    def size(self):
+        return self.__size
 
-	def size():
-		return self.size
-
-	def is_empty(self):
-		return self.size == 0
+    def is_empty(self):
+        return (self.back is None) or (self.back is None)
